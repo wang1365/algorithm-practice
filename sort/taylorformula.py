4d8fae17e1b3ef686d7afcadad878a7489cec1af
Xiaochuan Wang <xiaochuan.wang02@sap.com>
2017-06-12 10:49:37 +0800

    refactor taylor formula

--- sort/taylorformula.py
+++ sort/taylorformula.py
@@ -11,16 +11,28 @@ TAYLOR_N = 10
 def power_of_e(x):
     def f(x, n):
         factorial = 1
-        for i in range(1, int(n) + 1):
+        for i in range(1, n + 1):
             factorial = factorial * i
-        return float(x ** int(n)) / factorial
+        return float(x ** n) / factorial
 
-    ret = 1
-    for i in range(1, TAYLOR_N + 1):
-        ret += f(x, i)
+    # return sum(f(x, i) for i in range(0, TAYLOR_N + 1))
+    return sum(f(x, i) for i in range(0, 100 + 1))
 
-    return ret
     # return 1 + x + x ** 2 / 2 + x ** 3 / (2 * 3) + x ** 4 / (2 * 3 * 4) + x ** 5 / (2 * 3 ** 4 * 5)
+
+
+def sin(x):
+    def f(x, n):
+        if n % 2 == 0:
+            return 0
+        factorial = 1
+        for i in range(1, n + 1):
+            factorial = factorial * i
+
+        sign = -1 if ((n - 1) / 2) % 2 == 1 else 1
+        return sign * float(x ** n) / factorial
+
+    return sum(f(x, i) for i in range(0, TAYLOR_N + 1))
 
 
 if __name__ == '__main__':
@@ -29,3 +41,9 @@ if __name__ == '__main__':
     print math.e, power_of_e(1)
     print math.e ** 2, power_of_e(2)
     print math.e ** 3, power_of_e(3)
+    print math.e ** 1.89, power_of_e(1.89)
+
+    print 'sin(0):', math.sin(0), sin(0)
+    print 'sin(0.5):', math.sin(0.5), sin(0.5)
+    print 'sin(pi/2):', math.sin(math.pi/2), sin(math.pi/2)
+    print 'sin(pi):', math.sin(math.pi), sin(math.pi)
